ba91901110b63927a623653e78fe40ae147de780
Sławek Maciejewski <slawek74.m@interia.pl>
2020-12-13 13:36:02 +0000

    Added function which return random card from the List

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+
+# Function that uses List *cards* to *return* a random card.
+# 11 is the Ace.
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+    
+
