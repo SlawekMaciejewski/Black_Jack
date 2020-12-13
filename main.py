e9ebf9c0ba72be705e9fd06d0d72a8be9b3d16f3
Sławek Maciejewski <slawek74.m@interia.pl>
2020-12-13 14:09:02 +0000

    Added the function calculate_score() that takes a List of cards as input and returns the score.

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ for _ in range(2):
     computer_cards.append(deal_card())
 print(user_cards, computer_cards)
 
-
-    
+# Function calculate_score() that takes a List of cards as input 
+# and returns the score.
+def calculate_score(cards):
+    # 0 will represent a blackjack in our game
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    # 11 is Ace. If the score is over 21 Ace becomes 1
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)    
+    return sum(cards)
+print(calculate_score(user_cards))
 
